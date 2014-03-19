5a272be6327884c1cf710e498750713ef4cffa05
Giang Nguyen <giang.nguyentruong.uit@gmail.com>
2014-03-19 23:34:06 +0700

    + CustomAdapter will be replace for BaseAdapter

---
+++ src/com/talenguyen/androidframework/adapters/CustomAdapter.java
@@ -0,0 +1,70 @@
+package com.talenguyen.androidframework.adapters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.widget.BaseAdapter;
+
+public abstract class CustomAdapter<T> extends BaseAdapter {
+	
+	private List<T> data;
+	
+	public void add(T item) {
+		if (data == null) {
+			data = new ArrayList<T>();
+		}
+		data.add(item);
+	}
+	
+	public void remove(int position) {
+		if (data == null || data.size() <= position) {
+			return;
+		}
+		
+		data.remove(position);
+	}
+	
+	public void remove(T item) {
+		if (data == null || data.size() == 0) {
+			return;
+		}
+		
+		data.remove(item);
+	}
+	
+	public void changeDataSet(List<T> newData) {
+		if (data == newData) {
+			return;
+		}
+		
+		if (data != null && data.size() > 0) {
+			data.clear();
+			System.gc();
+		}
+		
+		data = newData;
+		notifyDataSetChanged();
+	}
+
+	@Override
+	public int getCount() {
+		if (data != null) {
+			return data.size();
+		}
+		return 0;
+	}
+
+	@Override
+	public T getItem(int position) {
+		if (data == null || data.size() <= position) {
+			return null;
+		}
+		return data.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+}
