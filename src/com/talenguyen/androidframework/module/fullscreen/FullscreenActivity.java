2b5c2d24bbe6141aaab30dfe7476f1ff971bdf32
Giang Nguyen <giang.nguyentruong.uit@gmail.com>
2014-04-15 23:13:59 +0700

    !update

--- src/com/talenguyen/androidframework/module/fullscreen/FullscreenActivity.java
+++ src/com/talenguyen/androidframework/module/fullscreen/FullscreenActivity.java
@@ -1,15 +1,12 @@
 package com.talenguyen.androidframework.module.fullscreen;
 
 import android.annotation.TargetApi;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
-import android.view.Window;
 import android.widget.FrameLayout;
 
 /**
@@ -19,23 +16,12 @@ import android.widget.FrameLayout;
  * @see SystemUiHider
  */
 public class FullscreenActivity extends FragmentActivity {
-	/**
-	 * Whether or not the system UI should be auto-hidden after
-	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-	 */
-	private static final boolean AUTO_HIDE = true;
 
 	/**
 	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
 	 * user interaction before hiding the system UI.
 	 */
-	private static final int AUTO_HIDE_DELAY_MILLIS = 100;
-
-	/**
-	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
-	 * will show the system UI visibility upon interaction.
-	 */
-	private static final boolean TOGGLE_ON_CLICK = true;
+	private static final int AUTO_HIDE_DELAY_MILLIS = 1000;
 
 	/**
 	 * The flags to pass to {@link SystemUiHider#getInstance}.
@@ -46,25 +32,20 @@ public class FullscreenActivity extends FragmentActivity {
 	 * The instance of the {@link SystemUiHider} for this activity.
 	 */
 	private SystemUiHider mSystemUiHider;
-	
-	protected FrameLayout mContentView;
+
+	private FrameLayout mRootView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-		mContentView = new FrameLayout(this);
-		mContentView.setBackgroundColor(Color.BLACK);
-		
-		mContentView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-		setContentView(mContentView);
+		mRootView = new FrameLayout(this);
+		setContentView(mRootView);
 
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
-		mSystemUiHider = SystemUiHider.getInstance(this, mContentView,
-				HIDER_FLAGS);
+		mSystemUiHider = SystemUiHider
+				.getInstance(this, mRootView, HIDER_FLAGS);
 		mSystemUiHider.setup();
 		mSystemUiHider
 				.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
@@ -72,28 +53,38 @@ public class FullscreenActivity extends FragmentActivity {
 					@Override
 					@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
 					public void onVisibilityChange(boolean visible) {
-						if (visible && AUTO_HIDE) {
+						if (visible) {
 							// Schedule a hide().
 							delayedHide(AUTO_HIDE_DELAY_MILLIS);
 						}
 					}
 				});
 
-//		// Set up the user interaction to manually show or hide the system UI.
-//		mContentView.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View view) {
-//				delayedHide(AUTO_HIDE_DELAY_MILLIS);
-//			}
-//		});
-
 	}
 	
 	@Override
-	protected void onPause() {
-		super.onPause();
-		mHideHandler.removeCallbacks(mHideRunnable);
-		mHideHandler.removeCallbacks(mFullscreenRunnable);
+	protected void onResume() {
+		super.onResume();
+		delayedHide(AUTO_HIDE_DELAY_MILLIS);
+	}
+
+	@Override
+	public void setContentView(int layoutResID) {
+		getLayoutInflater().inflate(layoutResID, mRootView, true);
+	}
+
+	@Override
+	public void setContentView(View view) {
+		if (view == mRootView) {
+			super.setContentView(view);
+		} else {
+			setContentView(view, null);
+		}
+	}
+
+	@Override
+	public void setContentView(View view, LayoutParams params) {
+		mRootView.addView(view, params);
 	}
 
 	@Override
@@ -103,49 +94,16 @@ public class FullscreenActivity extends FragmentActivity {
 		// Trigger the initial hide() shortly after the activity has been
 		// created, to briefly hint to the user that UI controls
 		// are available.
-		delayedHide(AUTO_HIDE_DELAY_MILLIS);
+		delayedHide(100);
 	}
-	
-	@Override
-	public void setContentView(int layoutResID) {
-		getLayoutInflater().inflate(layoutResID, mContentView);
-	}
-	
-	@Override
-	public void setContentView(View view, LayoutParams params) {
-		mContentView.addView(view, params);
-	}
-	
-	@Override
-	public void setContentView(View view) {
-		if (view == mContentView) {
-			super.setContentView(mContentView);
-		} else {
-			this.setContentView(view, null);
-		}
-	}
-	
 
 	Handler mHideHandler = new Handler();
 	Runnable mHideRunnable = new Runnable() {
 		@Override
 		public void run() {
 			mSystemUiHider.hide();
-			mHideHandler.removeCallbacks(mFullscreenRunnable);
-			mHideHandler.postDelayed(mFullscreenRunnable, 200);
 		}
 	};
-	Runnable mFullscreenRunnable = new Runnable() {
-		
-		@Override
-		public void run() {
-			onFullscreen();
-		}
-	};
-	
-	protected void onFullscreen() {
-		
-	}
 
 	/**
 	 * Schedules a call to hide() in [delay] milliseconds, canceling any
