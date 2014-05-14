05af8f184e43c2b8c3dfc9bbc96394d2e76dbc7e
Tale Nguyen <giangnguyen.tale@gmail.com>
2014-05-14 15:07:24 +0700

    ! SwipeBackLayout

---
+++ src/com/talenguyen/androidframework/views/SwipeBackLayoutUsage.java
@@ -0,0 +1,39 @@
+package com.talenguyen.androidframework.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by TALE on 5/14/2014.
+ */
+public class SwipeBackLayoutUsage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        SwipeBackLayout mSwipeBackLayout = (SwipeBackLayout) findViewById(R.id.swipe);
+//        mSwipeBackLayout.setContentView(findViewById(R.id.content_view));
+//        mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_ALL);
+//        mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
+//            @Override
+//            public void onScrollStateChange(int state, float scrollPercent) {
+//
+//            }
+//
+//            @Override
+//            public void onEdgeTouch(int edgeFlag) {
+//
+//            }
+//
+//            @Override
+//            public void onScrollOverThreshold() {
+//
+//            }
+//
+//            @Override
+//            public void onSwipeCompleted() {
+//                finish();
+//            }
+//        });
+    }
+}
